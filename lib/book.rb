33717a29e480437025096367a07adc5655fe700f
ChristopherVuvu <vuvumumo93@gmail.com>
2023-05-25 02:24:38 +0300

    added

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,18 @@
+class Book
+    attr_accessor :title, :author, :page_count, :genre
+    
+    def initialize(title, author, page_count, genre)
+        @title = title
+        @author = author
+        @page_count = page_count
+        @genre = genre
+    end
+    
+    def turn_page
+        # Implementation for turning the page goes here
+        puts "Flipping the page..."
+    end
+end
 
+book = Book.new("And Then There Were None", "Agatha Christie", 300, "Mystery")
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,18 @@
-# Make your shoe class here!
+class Shoe
+    attr_accessor :brand, :color, :size, :material, :condition
+    
+    def initialize(brand, color, size, material, condition)
+        @brand = brand
+        @color = color
+        @size = size
+        @material = material
+        @condition = "new"
+    end
+    
+    def cobble
+        puts "The shoe has been repaired."
+        @condition = "new"
+    end
+end
+
+shoe = Shoe.new("Adidas", "Black", 9, "Leather")
